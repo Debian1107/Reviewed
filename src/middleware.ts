7473ce14d5659e341dc994827d3176c805d2ddf9
lilnxxx <swastik7210@gmail.com>
2025-10-10 20:35:18 +0530

    added middleware bug fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,33 +1,11 @@
-"use server";
-// import { NextResponse } from "next/server";
-// import type { NextRequest } from "next/server";
-// import jwt from "jsonwebtoken";
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
 
-// const JWT_SECRET = process.env.JWT_SECRET || "fallback_secret";
-
-// export function middleware(req: NextRequest) {
-//   const token = req.cookies.get("auth_token")?.value;
-//   // console.log("Middleware check for token----------:", token);
-
-//   if (!token) {
-//     return NextResponse.redirect(new URL("/login", req.url));
-//   }
-
-//   return NextResponse.next();
-//   // try {
-//   //   jwt.verify(token, JWT_SECRET);
-//   // } catch (err) {
-//   //   console.log("JWT verification failed:", err);
-//   //   return NextResponse.redirect(new URL("/login", req.url));
-//   // }
-// }
+export default withAuth({
+  pages: {
+    signIn: "/login",
+  },
+});
 
 export const config = {
-  matcher: [
-    "/reviews/:path*",
-    "/dashboard/:path*",
-    "/reviews",
-    "/submit-review",
-  ], // protected routes
+  matcher: ["/reviews/:path*", "/dashboard/:path*", "/submit-review"],
 };
